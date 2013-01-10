158290f7df1e1624a55819bae4f37d9d1c40b838
Tajima Junpei <p.baleine@gmail.com>
2013-01-10 14:31:38 +0900

    add spec for `to` and `from`

--- test/expect-change.js
+++ test/expect-change.js
@@ -4,19 +4,57 @@ describe("expect-change", function() {
     expect(expect().change).to.be.a(Function);
   });
 
-  it("should not raise error when there is change", function() {
-    expect(function() {
-      var cnt = 0;
-      expect(function() { cnt = 101; }).to
+  describe("#change()", function() {
+    it("should not raise error when there is change", function() {
+      expect(function() {
+        var cnt = 0;
+        expect(function() { cnt = 101; }).to
           .change(function() { return cnt; });
-    }).to.not.throwException();;
+      }).to.not.throwException();;
+    });
+
+    it("should raise error when there is no change", function() {
+      expect(function() {
+        var cnt = 0;
+        expect(function() { cnt = 0; }).to
+          .change(function() { return cnt; });
+      }).to.throwException();;
+    });
   });
 
-  it("should raise error when there is no change", function() {
-    expect(function() {
-      var cnt = 0;
-      expect(function() { cnt = 0; }).to
-          .change(function() { return cnt; });
-    }).to.throwException();;
+  describe("#to()", function() {
+    it("should not raise error when actual value is changed to expected value", function() {
+      expect(function() {
+        var cnt = 0;
+        expect(function() { cnt = 101; }).to
+          .change(function() { return cnt; }).to(101);
+      }).to.not.throwException();
+    });
+
+    it("should raise error when actual value is not changed to expected value", function() {
+      expect(function() {
+        var cnt = 0;
+        expect(function() { cnt = 0; }).to
+          .change(function() { return cnt; }).to(101);
+      }).to.throwException();
+    });
+  });
+
+  describe("#from()", function() {
+    it("should not raise error when actual value is changed from expected value", function() {
+      expect(function() {
+        var cnt = 0;
+        expect(function() { cnt = 101; }).to
+          .change(function() { return cnt; }).from(0);
+      }).to.not.throwException();
+    });
+
+    it("should raise error when actual value is not changed from expected value", function() {
+      expect(function() {
+        var cnt = 0;
+        expect(function() { cnt = 101; }).to
+          .change(function() { return cnt; }).from(10);
+      }).to.throwException();
+    });
   });
 });
